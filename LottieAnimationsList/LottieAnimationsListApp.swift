9746388814d7c167356b3abba05a9c03ad4ee02e
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-09 15:07:41 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
